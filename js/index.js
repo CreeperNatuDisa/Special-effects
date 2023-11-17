03c56d44f0d1beb273fa7ecf370d9bf5f1400609
CreeperNatuDisa <3140969720@qq.com>
2023-11-17 16:43:07 +0800

    删除了倒计时，修改了index.html在电脑和手机的样式

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function initSakanaWidget() {
-    new SakanaWidget().mount("#sakana-widget-chisato");
-    new SakanaWidget({ character: 'takina' }).mount("#sakana-widget-takina");
+    new SakanaWidget({ autoFit: true }).mount("#sakana-widget-chisato");
+    new SakanaWidget({ character: 'takina', autoFit: true }).mount("#sakana-widget-takina");
 }
 const vm = new Vue({
     el: '#box',
     data: {
-        fileList: ['下雪', '价钱选择', '价钱选择突出显示', '倒计时', '嘉城快乐修机小训练', '圆形进度条', '弹性扩展板', '彩色登录页', '手写签名', '文字上浮显示', '文字剪纸', '文字弹跳', '文字扩散', '旋转加载动画', '流星雨', '滑动跟踪导航栏', '粒子特效', '运动背景', '鼠标移入移出文字过渡效果']
+        fileList: ['下雪', '价钱选择', '价钱选择突出显示', '嘉城快乐修机小训练', '圆形进度条', '弹性扩展板', '彩色登录页', '手写签名', '文字上浮显示', '文字剪纸', '文字弹跳', '文字扩散', '旋转加载动画', '流星雨', '滑动跟踪导航栏', '粒子特效', '运动背景', '鼠标移入移出文字过渡效果']
     },
     created() {
 
